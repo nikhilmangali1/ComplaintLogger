aeab0f62aef5bcb5c151716090e032e2c45054a6
Nikhil Mangali <nikhilmangali158@gmail.com>
2025-05-27 16:44:32 +0530

    Initial commit

---
+++ src/main/java/com/nikhilmangali1/ComplaintLogger/ComplaintLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.nikhilmangali1.ComplaintLogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikhilmangali1/ComplaintLogger/ComplaintLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilmangali1.ComplaintLogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
